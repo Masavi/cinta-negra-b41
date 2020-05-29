c20a80dc64adc82a6b481b83d89a34fd5a50247a
maui <maui@devf.mx>
2020-05-28 21:09:34 -0500

    Instalar Express y configurar una API básica

--- 03-Base-De-Datos/index.js
+++ 03-Base-De-Datos/index.js
@@ -1,12 +1,29 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const server = express();
+const PORT = 6000;
 
 // Tomar la URL de conexion de Mongo Atlas
 // Sustituir usuario y contraseña en esa URL
 const MONGO_URI = 'mongodb://maui:abc123def@cinta-negra-shard-00-00-efwi5.mongodb.net:27017,cinta-negra-shard-00-01-efwi5.mongodb.net:27017,cinta-negra-shard-00-02-efwi5.mongodb.net:27017/test?ssl=true&replicaSet=cinta-negra-shard-0&authSource=admin&retryWrites=true&w=majority';
+// Conexión a Mongo Atlas
 mongoose.connect(MONGO_URI)
   .then(() => console.log('Conexion exitosa'))
   .catch(() => console.log('Error en la URI de conexion'));
 
+// Configuración de middlewares
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json({ extended: true }));
+
 /*
   OBJETIVO: CREAR UN CRUD DE UNA COLECCIÓN BÁSICA
 */
+
+// Endpoints
+server.get('/', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
+
+server.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
