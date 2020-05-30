ff02ea6d496c7a707966980e4e3a5e8e187a7bdd
maui <maui@devf.mx>
2020-05-29 21:39:00 -0500

    Agregar endpoint para crear productos

--- 04-Mongo-Referencias/api/index.js
+++ 04-Mongo-Referencias/api/index.js
@@ -12,4 +12,14 @@ server.get('/', (req, res) => {
 // Products
 const Products = require('../models/Products');
 
+server.post('/api/products', (req, res) => {
+    const { body } = req;
+    // Products.create
+    const newProduct = new Products(body);
+
+    newProduct.save()
+      .then(dbRes => res.status(201).json(dbRes))
+      .catch(err => res.status(400).json(err));
+});
+
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
