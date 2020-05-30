9573003198cb8496058232917e38dceb715a27cc
maui <maui@devf.mx>
2020-05-29 20:35:09 -0500

    Refactorizar conexion a base de datos en su propia carpeta

--- 04-Mongo-Referencias/index.js
+++ 04-Mongo-Referencias/index.js
@@ -1,8 +1,1 @@
-const mongoose = require('mongoose');
-const MONGO_URI = 'mongodb://maui:abc123def@cinta-negra-shard-00-00-efwi5.mongodb.net:27017,cinta-negra-shard-00-01-efwi5.mongodb.net:27017,cinta-negra-shard-00-02-efwi5.mongodb.net:27017/test?ssl=true&replicaSet=cinta-negra-shard-0&authSource=admin&retryWrites=true&w=majority';
-
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('Database connected'))
-  .catch(() => console.log('Error connecting to database'));
+require('./database');
