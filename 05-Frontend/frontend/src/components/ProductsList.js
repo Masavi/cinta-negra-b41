8adbdf7f0dbcd5508cc5bcbfbabdabe933421d3a
maui <maui@devf.mx>
2020-06-02 21:29:40 -0500

    Hacer peticion de Products y renderizar lista de Products

--- 05-Frontend/frontend/src/components/ProductsList.js
+++ 05-Frontend/frontend/src/components/ProductsList.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Product from './Product';
+import axios from 'axios';
 
 const ProductsList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:4040/api/products')
+      .then((res) => setProducts(res.data))
+      .catch((err) => console.log(err));
+  }, [])
+
   return ( 
     <React.Fragment>
       <h2>Products</h2>
-      <Product
-        name="Sopa Nissin"
-        price={10.50} />
+      { products.map(product => <Product key={product._id} name={product.name} price={product.price} />) }
     </React.Fragment>
   );
 }
