f1fdbb8bcacdd673ac0162d04165df35091cc63b
maui <maui@devf.mx>
2020-06-02 21:29:26 -0500

    Instalar y configurar CORS

--- 04-Mongo-Referencias/api/index.js
+++ 04-Mongo-Referencias/api/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const server = express();
 const path = require('path');
+const cors = require('cors');
 const PORT = 4040;
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json({ extended: true }));
+server.use(cors());
 
 server.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
