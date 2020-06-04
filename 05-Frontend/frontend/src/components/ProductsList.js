147e709ea42667b104a23100bf621aaf870bbc44
maui <maui@devf.mx>
2020-06-03 20:08:03 -0500

    Maquetar formulario de creacion de producto

--- 05-Frontend/frontend/src/components/ProductsList.js
+++ 05-Frontend/frontend/src/components/ProductsList.js
@@ -15,6 +15,34 @@ const ProductsList = () => {
     <React.Fragment>
       <h2>Products</h2>
       { products.map(product => <Product key={product._id} name={product.name} price={product.price} />) }
+      <div style={{
+        backgroundColor: "#5672f0",
+        padding: "1em",
+        width: "300px",
+      }}>
+        <h2 style={{ color: "white" }}>
+          Create Product
+        </h2>
+        <form style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}>
+          <input
+            placeholder="Name"
+            style={{ marginBottom: "1em"}}
+            type="text"></input>
+          <input
+            placeholder="Price"
+            style={{ marginBottom: "1em"}}
+            type="number"></input>
+          <button
+            style={{
+              color: "white",
+              backgroundColor: "navy"
+            }}>Create</button>
+        </form>
+      </div>
     </React.Fragment>
   );
 }
