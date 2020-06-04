ae0a197b9de295da341902d7a8887f4e24021beb
maui <maui@devf.mx>
2020-06-03 20:59:38 -0500

    Conectar interfaz con el estado

--- 05-Frontend/frontend/src/components/NewProductForm.js
+++ 05-Frontend/frontend/src/components/NewProductForm.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const NewProductForm = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, price);
+  }
+
   return ( 
     <aside style={{
       backgroundColor: "#5672f0",
@@ -11,20 +19,28 @@ const NewProductForm = () => {
       <h2 style={{ color: "white" }}>
         Create Product
       </h2>
-      <form style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-      }}>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
         <input
           placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           style={{ marginBottom: "1em"}}
           type="text"></input>
         <input
           placeholder="Price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
           style={{ marginBottom: "1em"}}
           type="number"></input>
         <button
+          type="submit"
           style={{
             color: "white",
             backgroundColor: "navy"
